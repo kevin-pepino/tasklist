f49303df12b04aaaa95303dc9b84a0a96895e870
Victor Falcon <victorfalcon99@gmail.com>
2024-09-30 13:57:21 +0200

    Fin dia 1

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,2 +1,50 @@
-function addTaskRedirect(){location.replace('insertTask.html');}
-function taskListRedirect(){location.replace('index.html');}
+/**************************/
+/* CREATED ON: 30/09/2024 */
+/**************************/
+/* AUTHORS:               */
+/*    Victor F.           */
+/*    Aaron F.            */
+/*    Xavier B.           */
+/*    Xavier R.           */
+/**************************/
+
+
+//HTML REFERENCES
+const tasksHTML = document.getElementById('tasks');
+
+//LOCAL STORAGE REFERENCES
+const tasksLS = localStorage.getItem('tasks');
+
+//localStorage.setItem('tasks', ['one', 'two', 'three']); 
+
+//REDIRECTS
+function addTaskRedirect(){location.href = '../HTML/insertTask.html';}
+function taskListRedirect(){location.href = '../HTML/index.html';}
+
+
+//GENERATE LIST ON MAIN PAGE
+try
+{        
+    if(tasksLS == null)
+    {
+        tasksHTML.innerHTML = `<p>No hay tareas añadidas en el listado. Empieza a añadir unas cuantas</p><button onclick="addTaskRedirect()">Añadir Tarea</button>`;
+    }
+    else
+    {
+        let ray = tasksLS.split(',');
+        for(i = 0; i < ray.length; i++)
+        {
+            tasksHTML.innerHTML += `<div id='task'>${ray[i]}</div>`;
+        }
+    }
+}
+catch
+{
+    console.log('Outside of List Site');
+}
+
+//ADD TASK TO THE LIST
+function addTask()
+{
+    location.href = '../HTML/index.html';
+}
