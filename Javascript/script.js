d5c45ff52ba5f8cc250ba49b19540b9239eff0be
Victor Falcon <victorfalcon99@gmail.com>
2024-10-01 13:51:45 +0200

    Fin dia 2

--- Javascript/script.js
+++ Javascript/script.js
@@ -8,15 +8,6 @@
 /*    Xavier Ramirez      */
 /**************************/
 
-
-/*const ite = {
-    title: 'kek', 
-    desc: 'kekeroni', 
-    timestamp: Date.now(), 
-    isFinished: true
-};
-console.log(ite);*/
-
 //HTML REFERENCES
 const tasksHTML = document.getElementById('tasks');
 
@@ -26,72 +17,132 @@ const tasksLS = localStorage.getItem('tasks');
 //FOR LOCAL STORAGE WIPE
 function fullwipe()
 {
-    localStorage.clear(); 
+    localStorage.clear();
     location.reload();
 }
-
-//console.log(tasksLS);
 
 //REDIRECTS
 function addTaskRedirect(){location.href = '../HTML/insertTask.html';}
 function taskListRedirect(){location.href = '../HTML/index.html';}
-
+function editTaskRedirect(){location.href = '../HTML/editTask.html';}
 
 //GENERATE LIST ON MAIN PAGE
-//try
-//{        
-    if(tasksLS == null)
-    {
-        tasksHTML.innerHTML = `<p>No hay tareas añadidas en el listado. Empieza a añadir unas cuantas</p><button onclick="addTaskRedirect()">Añadir Tarea</button>`;
-    }
-    else
-    {
-        let list = JSON.parse(tasksLS);
-        console.log(list);
-        for(i = 0; i < list.length; i++)
-        {
-            tasksHTML.innerHTML += `
-            <div id='task'>
-                <tr>${list[i].title}</tr>
-                <tr>${list[i].desc}</tr>
-                <tr>${list[i].timestamp}</tr>
-                <tr>${list[i].isFinished}</tr>
-            </div>
-            `;
-        }
-    }
-//}
-/*catch
+if(tasksLS == null)
 {
-    console.log('Outside of List Site');
-}*/
+    tasksHTML.innerHTML = `<p>No hay tareas añadidas en el listado. Empieza a añadir unas cuantas</p><button onclick="addTaskRedirect()">Añadir Tarea</button>`;
+}
+else
+{
+    let list = JSON.parse(tasksLS);
+    console.log(list);
+    tasksHTML.innerHTML += `
+        <div id='taskTitles' class='taskTitles'>
+            <p>Titulo</p>
+            <p>Descripcion</p>
+            <p>Creado</p>
+            <p>Estado</p>
+        </div>
+    `;
+    for(i = 0; i < list.length; i++)
+    {
+        tasksHTML.innerHTML += `
+        <div id='task' class='task'>
+            <p>${list[i].title}</p>
+            <p>${list[i].desc}</p>
+            <p>${list[i].timestamp}</p>
+            <p>${list[i].isFinished}</p>
+            <button onclick="finishTask('${list[i].id}')">Finalizar</button>
+            <button onclick="editTaskRedirect('${list[i].id}')">Modificar</button>
+            <button onclick="deleteTask('${list[i].id}')">Eliminar</button>
+            <!--p>${list[i].id}</p-->
+        </div>
+        `;
+    }
+    tasksHTML.innerHTML += '</div>';
+}
+
 
 //ADD TASK TO THE LIST
 function addTask()
 {
     let list = [];
-    if(tasksLS != null){list = JSON.parse(tasksLS);}
-    let title = document.forms['form']['title'].value;
-    let desc = document.forms['form']['desc'].value;
-    let timestamp = Date.now();
-    let isFinished = false;
+    if(tasksLS != null){list = JSON.parse(tasksLS);}            
+
+    let id = Math.floor(Math.random(0, 1) * 10000000000);       //RANDOMLY GENERATED ID
+    let title = document.forms['form']['title'].value;          //TITLE OF THE TASK
+    let desc = document.forms['form']['desc'].value;            //DESCRIPTION
+    let timestamp = today();                                    //TIME STAMP OF WHEN IT WAS CREATED
+    let isFinished = false;                                     //TASK STATUS, SET AS INCOMPLETE (FALSE)
     const item = {
+        id: id,
         title: title, 
         desc: desc, 
         timestamp: timestamp, 
         isFinished: isFinished
     };
     console.log('item');
-    list.push(item);
-    localStorage.setItem('tasks', JSON.stringify(list));
-    location.href = '../HTML/index.html';
+    //if(title == null){}
+    /*if(repeatCheck(title))
+    {
+        alert("The following title is already present on the list");
+    }
+    else
+    {*/
+        list.push(item);
+        localStorage.setItem('tasks', JSON.stringify(list));
+        location.href = '../HTML/index.html';
+    //}
 }
 
-/*function editTask()
+function today()
 {
-    let list = tasksLS.split(',');
-    for(i = 0; i < list.length; i++)
-    {
-        
-    }
-}*/
+    let dt = new Date()
+    return `${dt.getDate()}/${dt.getMonth() + 1}/${dt.getFullYear()}`;
+}
+
+//RETURNS WHETHER THE TITLE IS ALREADY PRESENT IN THE LIST OR NOT
+function repeatCheck(title)
+{
+    JSON.parse(tasksLS).forEach(element => {
+        if(element.title == title)
+        {
+            return true;
+        }
+    });
+    return false;
+}
+
+//SETS THE VALUE ISFINISHED AS TRUE, FINISHING THEREFORE THE TASK
+function finishTask(id)
+{
+    let list = JSON.parse(tasksLS);
+    list.forEach(element => {
+        if(element.id == id)
+        {
+            element.isFinished = true;
+        }
+    });
+    localStorage.setItem('tasks', JSON.stringify(list));
+    location.reload();
+}
+
+function editTask(id)
+{
+    
+}
+
+//DELETE A SPECIFIC TASK
+function deleteTask(id)
+{
+    let list = JSON.parse(tasksLS);
+    let newList = [];
+    list.forEach(element => {
+        if(element.id != id)
+        {
+            newList.push(element);
+        }
+    });
+    if(newList.length == 0){fullwipe();}
+    else{localStorage.setItem('tasks', JSON.stringify(newList));}
+    location.reload();
+}
