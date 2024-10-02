d7b101a6ef4de3c03edd3ccb09fbbf37e1d1fee5
Victor Falcon <victorfalcon99@gmail.com>
2024-10-02 13:30:03 +0200

    Editar tarea

--- Javascript/script.js
+++ Javascript/script.js
@@ -12,7 +12,8 @@
 const tasksHTML = document.getElementById('tasks');
 
 //LOCAL STORAGE REFERENCES
-const tasksLS = localStorage.getItem('tasks');
+const tasksLS = localStorage.getItem('tasks');      //ALL TASKS SAVED
+const idLS = localStorage.getItem('id');          //ID SHARED ACROSS PAGES
 
 //FOR LOCAL STORAGE WIPE
 function fullwipe()
@@ -24,45 +25,58 @@ function fullwipe()
 //REDIRECTS
 function addTaskRedirect(){location.href = '../HTML/insertTask.html';}
 function taskListRedirect(){location.href = '../HTML/index.html';}
-function editTaskRedirect(item)
+function editTaskRedirect(id)
 {
+    localStorage.setItem('id', id);
     location.href = '../HTML/editTask.html';
-    document.getElementById('ctTitle').innerText = item.title;
-    document.getElementById('ctDate').innerText = item.datelimit;
 }
 
 //GENERATE LIST ON MAIN PAGE
-if(tasksLS == null)
+function loadList()
 {
-    tasksHTML.innerHTML = `<p class="txt">No hay tareas añadidas en el listado. Empieza a añadir unas cuantas</p><button onclick="addTaskRedirect()">Añadir Tarea</button>`;
-}
-else
-{
-    let list = JSON.parse(tasksLS);
-    //console.log(list);
-    tasksHTML.innerHTML += `
-        <div id='taskTitles' class='taskTitles'>
-            <p>Titulo</p>
-            <p>Fecha Limite</p>
-            <p>Estado</p>
-        </div>
-    `;
-    for(i = 0; i < list.length; i++)
+    if(tasksLS == null)
     {
-        tasksHTML.innerHTML += `
-        <div id='task' class='task'>
-            <p>${list[i].title}</p>
-            <p>${list[i].datelimit}</p>
-            <p>${list[i].isFinished}</p>
-            <button id="fin" class="btn" onclick="finishTask('${list[i].id}')">Finalizar</button>
-            <button id="mod" class="btn" onclick="editTaskRedirect('${list[i].id}')">Modificar</button>
-            <button id="sup" class="btn" onclick="deleteTask('${list[i].id}')"><img src="../img/CuboBasura.png" width=50 height=50></button>
-        </div>
-        `;
+        tasksHTML.innerHTML = `<p class="txt">No hay tareas añadidas en el listado. Empieza a añadir unas cuantas</p><button onclick="addTaskRedirect()">Añadir Tarea</button>`;
     }
-    tasksHTML.innerHTML += '</div>';
+    else
+    {
+        let list = JSON.parse(tasksLS);
+        //console.log(list);
+        tasksHTML.innerHTML += `
+            <div id='taskTitles' class='taskTitles'>
+                <p>Titulo</p>
+                <p>Fecha Limite</p>
+                <p>Estado</p>
+            </div>
+        `;
+        for(i = 0; i < list.length; i++)
+        {
+            tasksHTML.innerHTML += `
+            <div id='task' class='task'>
+                <p>${list[i].title}</p>
+                <p>${reformat(list[i].datelimit)}</p>
+                <p>${list[i].isFinished}</p>
+                <button id="fin" class="btn" onclick="finishTask('${list[i].id}')">Finalizar</button>
+                <button id="mod" class="btn" onclick="editTaskRedirect('${list[i].id}')">Modificar</button>
+                <button id="sup" class="btn" onclick="deleteTask('${list[i].id}')"><img src="../img/CuboBasura.png" width=50 height=50></button>
+            </div>
+            `;
+        }
+        tasksHTML.innerHTML += '</div>';
+    }
 }
 
+//INSERTS ITEM INFO WHEN LOADING THE EDIT TASK
+function loadEdit()
+{
+    JSON.parse(tasksLS).forEach(element => {
+        if(element.id == idLS)
+        {
+            document.getElementById('ctTitle').value = element.title;
+            document.getElementById('ctDate').value = element.datelimit;
+        }
+    });
+}
 
 //ADD TASK TO THE LIST
 function addTask()
@@ -75,8 +89,7 @@ function addTask()
     let datelimit = document.forms['form']['datelimit'].value;  //DATE LIMIT
     let timestamp = today();                                    //TIME STAMP OF WHEN IT WAS CREATED
     let isFinished = false;                                     //TASK STATUS, SET AS INCOMPLETE (FALSE)
-
-    datelimit = reformatDate(datelimit);
+    
 
     const item = {
         id: id,
@@ -86,10 +99,7 @@ function addTask()
         isFinished: isFinished
     };
     if(title == ''){alert('El titulo no puede estar vacío.');}
-    /*if(repeatCheck(title))
-    {
-        alert("Ya se ha introducido una tarea con este título.");
-    }*/
+    else if(datelimit == ''){alert('La fecha no puede estar vacía.');}
     else
     {
         list.push(item);
@@ -99,7 +109,7 @@ function addTask()
 }
 
 //CHANGE THE DISPLAY OF DATELIMIT
-function reformatDate(dl)
+function reformat(dl)
 {
     let day = dl.split('-')[2];
     if(day[0] == '0'){day = day[1];}
@@ -122,12 +132,15 @@ function today()
 //RETURNS WHETHER THE TITLE IS ALREADY PRESENT IN THE LIST OR NOT
 function repeatCheck(title)
 {
-    JSON.parse(tasksLS).forEach(element => {
-        if(element.title == title)
-        {
-            return true;
-        }
-    });
+    if(tasksLS != null)
+    {
+        JSON.parse(tasksLS).forEach(element => {
+            if(element.title == title)
+            {
+                return true;
+            }
+        });
+    }
     return false;
 }
 
@@ -145,23 +158,20 @@ function finishTask(id)
     location.reload();
 }
 
-//REFORMAT DATE FOR DISPLAY
-function reformatDate(dl)
+//UPDATE A TASK WITH NEW INFO
+function editTask()
 {
-    let day = dl.split('-')[2];
-    if(day[0] == '0'){day = day[1];}
-
-    let month = dl.split('-')[1];
-    if(month[0] == '0'){month = month[1];}
-
-    let year = dl.split('-')[0];
-
-    return `${day}/${month}/${year}`;
-}
-
-function editTask(id)
-{
-    
+    let list = [];
+    JSON.parse(tasksLS).forEach(element => {
+        if(element.id == idLS)
+        {
+            element.title = document.forms['form']['newTitle'].value;
+            element.datelimit = document.forms['form']['newDate'].value;;
+        }
+        list.push(element)
+    });
+    localStorage.setItem('tasks', JSON.stringify(list));
+    location.href = '../HTML/index.html';
 }
 
 //DELETE A SPECIFIC TASK
